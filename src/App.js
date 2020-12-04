2447789876d51c008b1058c473904f3cbbde2c9d
ardidwi <ardidwiwork@gmail.com>
2020-12-04 21:28:06 +0700

    komit pertama

---
+++ src/App.js
@@ -0,0 +1,93 @@
+
+import React, { Component } from 'react'
+import Barang from './component/Barang'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import MenuItem from './component/MenuItem'
+import MenuSlider from './component/MenuSlider'
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      datas: [],
+      getData: []
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData() {
+    let api_endpoint_search = "https://fakestoreapi.com/products";
+    fetch(api_endpoint_search)
+      .then(response => {
+
+
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+
+      }).then(response => {
+        this.setState({
+          datas: response,
+        });
+        console.log(response);
+
+      }).catch((reject) => {
+        alert(reject);
+      });
+  }
+
+  render() {
+    const {
+      title,
+      category,
+      price,
+      totalPrice,
+      description,
+      getData,
+      datas,
+      key,
+      image
+    } = this.state;
+    return (
+      <div>
+        <Header />
+        <MenuSlider datas={datas} />
+        <MenuItem />
+        <div className="container">
+          <div className="row" style={{ "marginTop": "30px", "marginLeft": "20px" }}>
+
+            {
+              datas.map(element => (
+                <div className="col-md-3">
+                  <Barang
+                    key={element.id}
+                    id={element.id}
+                    title={element.title}
+                    image={element.image}
+                    price={element.price}
+                    category={element.category}
+                    description={element.description}
+                  />
+                </div>
+              ))
+            }
+
+          </div>
+        </div>
+
+        <Footer />
+      </div >
+    )
+  }
+}
+
+
+export default App
